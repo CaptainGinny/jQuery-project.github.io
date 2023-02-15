f712a346c67c754ca834156881e12294619ae5d4
CaptainGinny <ginnymalko@gmail.com>
2023-02-15 12:37:53 +0300

    final

--- main.js
+++ main.js
@@ -43,18 +43,6 @@ $(document).ready(function() {
     modelSpecsHolder.text( modelSpecs );
   };
 
-  function addSpace(nStr) { 
-    nStr += '';
-    x = nStr.split('.');
-    x1 = x[0];
-    x2 = x.length > 1 ? '.' + x[1] : '';
-    let rgx =/(\d+)(\d{3})/;
-    while(rgx.test(x1)) {
-      x1 = x1.replace(rgx, '$1' + '' + '$2');
-    };
-    return x1 + x2;
-  };
-
 //Получить курс валют
 
   let currencyUrl= 'https://www.nbrb.by/api/exrates/rates/431';
